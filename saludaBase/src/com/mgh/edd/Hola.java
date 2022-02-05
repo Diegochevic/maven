92b36b606d524e4147b4780b8a7a9b0365501562
Diego <diegoisspro@gmail.com>
2022-02-05 15:41:42 +0100

    quitada carpeta original

--- saludaBase/src/com/mgh/edd/Hola.java
+++
@@ -1,8 +0,0 @@
-package com.mgh.edd;
-
-public class Hola {
-    public static void main(String[] args) {
-        String nombre=args.length>0?args[0]:"";
-        Saluda.saluda(nombre);
-    }
-}
--- saludaBase/src/com/mgh/edd/Saluda.java
+++
@@ -1,7 +0,0 @@
-package com.mgh.edd;
-
-public class Saluda {
-    public static void saluda(String nombre) {
-        System.out.println("Hola "+nombre);
-    }
-}
